796b14c096140f4cc14a6cce108f0f373cd02df6
janaka120 <janakastill@gmail.com>
2023-07-29 17:56:45 +0800

    Add notification feature

---
+++ app/src/main/java/com/example/registeruserapplication/MyFirebaseService.kt
@@ -0,0 +1,12 @@
+package com.example.registeruserapplication
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+    }
+}
